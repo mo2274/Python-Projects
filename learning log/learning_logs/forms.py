80ba040de8e9d847d419867aa78645fb3ef5ca70
Mohamed Abo Raia <mohamedzaghlol74111@gmail.com>
2021-01-11 03:08:24 +0200

    add styling to the project

--- learning log/learning_logs/forms.py
+++ learning log/learning_logs/forms.py
@@ -6,12 +6,12 @@ class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
-        labels = {'text': ''}
+        labels = {'text': 'Topic Title'}
 
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = ['text']
-        labels = {'text': 'Entry:'}
+        labels = {'text': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
